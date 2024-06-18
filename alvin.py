9103991d9bd9dda55619c5db7443e5a81e31d9b1
unknown <daquizalvin8@gmail.com>
2024-06-18 23:23:21 +0800

    version 2

---
+++ alvin.py
@@ -0,0 +1,28 @@
+for x in range(1, 11,):
+    if x == 8:
+        break
+    else:
+        print(x)
+
+
+
+
+
+
+#credit_card = "1234-423423-44"
+#for x in credit_card:
+#    print(x)
+
+
+
+
+
+
+
+
+
+
+
+#for x in range(1,11, 3):
+#    print(x)
+#print("Happy  Birthday:")
